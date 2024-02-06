bec032aeea33bb1abd64086d1d477c1fb90480c6
Umayanga Madushan <40480717+UtopiaLab@users.noreply.github.com>
2024-02-06 23:01:17 +0530

    initial settings

---
+++ ai-media-player.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Plugin Name: AI Media Player
+Description: Media player for Loudmusic AI audio generation playbacks.
+Version: 1.0.0
+Author: Umayanga Madushan
+Author URI: https://facebook/umayangalk
+License: GPL-2.0+
+GitHub Plugin URI: UtopiaLab/AI-Media-Player
+*/
+
+// Your plugin code starts here
+// ...
+
+// End of your plugin code
+?>
